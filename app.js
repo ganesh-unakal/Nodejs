e75d5189b544eeef86a48d15f2f99c348b4ebd10
Ganesh Unakal <122563639+ganesh-unakal@users.noreply.github.com>
2023-10-06 08:24:36 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,46 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
    const url = req.url;
+   const method = req.method;
+
    if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body> <form action="/message" methods="POST"> <input type="text" name="message"> <button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end();// this for not execute else Part
-   }
-
-   if(url==='/home') {
-      res.write('<html>');
-      res.write('<head><title>Home Page</title></head>');
-      res.write('<body> <h1>Welcome Home</h1></body>');
-      res.write('</html>');
-      return res.end();
-   }
-
-   if(url==='/about') {
-      res.write('<html>');
-      res.write('<head><title>about page</title></head>');
-      res.write('<body><h1>Welcome to About Us Page</h1></body>');
-      res.write('</html>');
-      return res.end();
-   }
-
-   if(url=== '/node') {
-      res.write('<html>')
-      res.write('<head><title>node page</title></head>');
-      res.write('<body><h1>Welcome to my node js project</h1></body>')
-      res.write('</html>')
-      return res.end();
+      fs.readFile('message.txt',{encoding : 'utf-8'}, (err,data)=>{
+         if (err) {
+            console.log(err)
+         }
+         
+         console.log(`data fetched, ${data}`);
+         res.write('<html>');
+         res.write('<head><title>Read and write from file</title></head>');
+         res.write('<body>');
+         res.write(`<h3>previous data : ${data} </h3>`)
+         res.write(
+            `<form action='/message' method='POST'>
+            <input type='text' name='message'><button type ='submit'>send</button></form>`
+          );
+          res.write('</body>');
+          res.write('</html>');
+          return res.end();
+      })
    }
 
 
-   res.setHeader('Content-Type', 'text/html');
-   res.write('<html>');
-   res.write('<head><title> My First page </title></head>');
-   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-   res.write('</html>');
-   res.end();
+   if(url==="/message" && method==="POST") {
+      const body =[]
+      req.on('data',(chunk)=>{
+         console.log(chunk)
+         body.push(chunk)
+      })
+     req.on('end',()=>{
+         const parsedBody = Buffer.concat(body).toString();
+         const message = parsedBody.split('=')[1];//1 is the 2nd element in the resulting array
+         fs.writeFileSync('message.txt', message);//tz is depend on the incoming data so
+
+      });
+
+
+      res.statusCode=302; //3002 is stutus code which stands for redirection
+      res.setHeader('Location', '/');
+      return res.end()
+   }
 })
 
 server.listen(4000);
